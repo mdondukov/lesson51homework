630f5483fd0b9de7c6c12b4be948073610be87a8
Max <mdondukov@pm.me>
2020-03-11 17:03:18 +0600

    update Singer model

--- src/main/java/com/example/lesson51homework/model/Singer.java
+++ src/main/java/com/example/lesson51homework/model/Singer.java
@@ -4,10 +4,30 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Document(collection = "singers")
 @Data
 public class Singer {
     @Id
     private String singerId;
     private String name;
+    private List<Album> albums;
+
+    public Singer(String name) {
+        this.name = name;
+    }
+
+    public void addAlbum(Album album) {
+        if (albums == null) albums = new ArrayList<>();
+        albums.add(album);
+    }
+
+    @Override
+    public String toString() {
+        return "Singer{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
