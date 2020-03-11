e08e185c4087837d7ae2c4cdd0f07e9ffb9cfc47
Max <mdondukov@pm.me>
2020-03-11 21:20:31 +0600

    fix tests

--- src/main/java/com/example/lesson51homework/util/PreloadDatabaseWithData.java
+++ src/main/java/com/example/lesson51homework/util/PreloadDatabaseWithData.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
--- src/test/java/com/example/lesson51homework/Lesson51homeworkApplicationTests.java
+++ src/test/java/com/example/lesson51homework/Lesson51homeworkApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.lesson51homework;
 
+import com.example.lesson51homework.model.Singer;
+import com.example.lesson51homework.model.SingerRepository;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Lesson51homeworkApplicationTests {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Lesson51homeworkApplicationTests.class);
+
+    @Autowired
+    private SingerRepository singerRepository;
+
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testCountOfSingers() {
+        LOG.info("*** Number of Singers TEST ***");
+        LOG.info("Singers count: " + singerRepository.count());
+        LOG.info("******************************");
+    }
+
+    @Test
+    public void testFindAll() {
+        LOG.info("*** ALL SINGERS ***");
+
+        Iterable<Singer>  allSingers = singerRepository.findAll();
+        for (Singer singer : allSingers) {
+            LOG.info(singer.toString());
+        }
+
+        LOG.info("*******************");
+    }
 }
